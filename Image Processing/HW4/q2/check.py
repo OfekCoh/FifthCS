734b6eb808bb703a02e670b502342cd2e8b4efcf
Ofek <ofekcohen977@gmail.com>
2025-01-16 13:09:25 +0200

    .

---
+++ Image Processing/HW4/q2/check.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+canvas= np.zeros((50,50)) 
+canvas[25][25]=255
+
+plt.figure(figsize=(10,10))
+plt.subplot(321)
+plt.title('Original Grayscale Image')
+plt.imshow(canvas, cmap='gray')
+plt.show()
